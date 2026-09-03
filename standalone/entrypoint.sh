4817925c0eaeb99e00995dac64c19c1105489dad
eyezah (Gigabox) <50851883+eyezahhhh@users.noreply.github.com>
2026-09-04 10:41:22 +1200

    Fix internal volume paths

--- standalone/entrypoint.sh
+++ standalone/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 set -e
 
-mkdir -p /data /resources /audio-cache /plugin-cache /plugins /temp /.secrets
+mkdir -p /data /srv/api/resources /srv/api/audio-cache /srv/api/plugin-cache /srv/api/plugins /srv/api/temp /srv/api/.secrets
 
-chown pipebomb:pipebomb /data /resources /audio-cache /plugin-cache /plugins /temp /.secrets
-chmod 750 /data /resources /audio-cache
-chmod 777 /plugins /plugin-cache /temp
-chmod 700 /.secrets
+chown pipebomb:pipebomb /data /srv/api/resources /srv/api/audio-cache /srv/api/plugin-cache /srv/api/plugins /srv/api/temp /srv/api/.secrets
+chmod 750 /data /srv/api/resources /srv/api/audio-cache
+chmod 777 /srv/api/plugins /srv/api/plugin-cache /srv/api/temp
+chmod 700 /srv/api/.secrets
 
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/pipe-bomb.conf
